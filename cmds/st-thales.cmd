4d54158c755b42a1ff322e446283ce3157f59fd0
Rafael Montano <rafaelmontano@e3-ioc-rf-lab.cslab.esss.lu.se>
2019-09-03 10:34:30 +0200

    Included the FIL PSU

---
+++ cmds/st-thales.cmd
@@ -0,0 +1,60 @@
+#
+
+require s7plc,1.4.0p
+
+
+epicsEnvSet("TOP", "$(E3_CMD_TOP)/..")
+
+
+epicsEnvSet("PREFIX", "$(RFLPS_PREFIX=BENCH:RFS01)")
+epicsEnvSet("PLCIP", "$(RFLPS_IP=172.30.4.221)")
+
+#var s7plcDebug 5
+
+## Datablocks
+#epicsEnvSet("TCPPORTCPU", "3000")
+#epicsEnvSet("PLCPORTCPU", "PLCCPU")
+#epicsEnvSet("INSIZECPU", "8")
+#epicsEnvSet("OUTSIZECPU", "4")
+
+#epicsEnvSet("TCPPORTAF", "3001")
+#epicsEnvSet("PLCPORTAF", "PLCAF")
+#epicsEnvSet("INSIZEAF", "836")
+#epicsEnvSet("OUTSIZEAF", "494")
+
+#epicsEnvSet("TCPPORTDIO", "3002")
+#epicsEnvSet("PLCPORTDIO", "PLCDIO")
+#epicsEnvSet("INSIZEDIO", "408")
+#epicsEnvSet("OUTSIZEDIO", "68")
+
+epicsEnvSet("TCPPORTPSU", "3000")
+epicsEnvSet("PLCPORTPSU", "PLCPSU")
+epicsEnvSet("INSIZEPSU", "14")
+epicsEnvSet("OUTSIZEPSU", "10")
+
+#s7plcConfigure("$(PLCPORTCPU)","$(PLCIP)",$(TCPPORTCPU),$(INSIZECPU),$(OUTSIZECPU),1,2500,500)
+#s7plcConfigure("$(PLCPORTAF)","$(PLCIP)",$(TCPPORTAF),$(INSIZEAF),$(OUTSIZEAF),1,2500,500)
+#s7plcConfigure("$(PLCPORTDIO)","$(PLCIP)",$(TCPPORTDIO),$(INSIZEDIO),$(OUTSIZEDIO),1,2500,500)
+s7plcConfigure("$(PLCPORTPSU)","$(PLCIP)",$(TCPPORTPSU),$(INSIZEPSU),$(OUTSIZEPSU),1,2500,500)
+
+epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(TOP)/rflpslab-loc/rflpslabApp/Db/")
+
+#dbLoadTemplate("$(TOP)/template/rflpsCPU.substitutions", "PREFIX=$(PREFIX)")
+#dbLoadTemplate("$(TOP)/template/rflpsAF.substitutions", "PREFIX=$(PREFIX)")
+#dbLoadTemplate("$(TOP)/template/rflpsDIO.substitutions", "PREFIX=$(PREFIX)")
+dbLoadTemplate("$(TOP)/template/rflpsPSU.substitutions", "PREFIX=$(PREFIX)")
+
+# After installation.
+
+# dbLoadRecords("rflpsCPU.db", "PREFIX=$(PREFIX)")
+# dbLoadRecords("rlpsAF.db", "PREFIX=$(PREFIX)")
+# dbLoadRecords("rflpsDIO", "PREFIX=$(PREFIX)")
+# dbLoadRecords("rflpsPSU", "PREFIX=$(PREFIX)")
+
+
+
+iocInit
+
+
+dbl > "$(TOP)/$(PREFIX)_PVs.list"
+
